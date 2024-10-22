5a19ef056dc5bfc081fb4ba3d2cf022f44f04d23
Mazen-Serli <mazen.natour@serli.com>
2024-10-22 14:57:19 +0200

    Update init-models.js

--- database/models/init-models.js
+++ database/models/init-models.js
@@ -1,12 +1,12 @@
-var DataTypes = require("sequelize").DataTypes;
-var _flags = require("./flags");
-var _projects = require("./projects");
-var _users = require("./users");
+const DataTypes = require("sequelize").DataTypes;
+const _flags = require("./flags");
+const _projects = require("./projects");
+const _users = require("./users");
 
 function initModels(sequelize) {
-  var flags = _flags(sequelize, DataTypes);
-  var projects = _projects(sequelize, DataTypes);
-  var users = _users(sequelize, DataTypes);
+  const flags = _flags(sequelize, DataTypes);
+  const projects = _projects(sequelize, DataTypes);
+  const users = _users(sequelize, DataTypes);
 
   flags.belongsTo(projects, { as: "project", foreignKey: "projectid"});
   projects.hasMany(flags, { as: "flags", foreignKey: "projectid"});
