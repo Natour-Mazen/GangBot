b391ca5beeccabf8c6edbbcce56e57eb0861b602
Dylan <78911744+Didiloy@users.noreply.github.com>
2024-10-22 13:10:44 +0200

    redirect user when not authenticated

--- index.js
+++ index.js
@@ -14,11 +14,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
+app.use((req, res, next) => {
+    if (!req.cookies.access_token && req.path !== '/auth' && req.path !== '/auth/callback') {
+        return res.redirect('/auth');
+    }
+    next();
+})
 
 app.use('/auth', authRouter);
+
+
+app.get('/', async (req, res) => {
+    const { models } = await getDatabase();
+    const user = await models.users.findOne({
+        where: {
+            githubtoken: req.cookies.access_token
+        }
+    });
+    res.send(`Hello ${user.githubusername}`);
+})
+
 app.listen(port, async () => {
     const { sequelize, models } = await getDatabase();
-    console.log(await models.users.findAll());
     console.log(`Server listening on port ${port}`);
 });
 
