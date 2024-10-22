c354ec4c138c6e38f67eae99023b8505112aa200
Mazen-Serli <mazen.natour@serli.com>
2024-10-22 14:58:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const getDatabase = require('./database/config');
 const port = 3328;
 const authRouter = require('./auth');
+const authJWTMiddleware = require("./middlewares/authTokenJWT");
 const app = express();
 
 app.use(logger('dev'));
@@ -14,28 +15,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
-app.use((req, res, next) => {
-    if (!req.cookies.access_token && req.path !== '/auth' && req.path !== '/auth/callback') {
-        return res.redirect('/auth');
-    }
-    next();
-})
+
 
 app.use('/auth', authRouter);
 
 
-app.get('/', async (req, res) => {
-    const { models } = await getDatabase();
-    const user = await models.users.findOne({
-        where: {
-            githubtoken: req.cookies.access_token
-        }
-    });
-    res.send(`Hello ${user.githubusername}`);
+app.get('/', authJWTMiddleware() ,async (req, res) => {
+    const name = req.connectedUser.name;
+    res.send(`Hello ${name}`);
 })
 
 app.listen(port, async () => {
-    const { sequelize, models } = await getDatabase();
+    await getDatabase();
     console.log(`Server listening on port ${port}`);
 });
 
