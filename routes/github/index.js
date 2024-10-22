180b012f1a5230f07ea98f14a1ca3661d3fb47d6
Mazen-Serli <mazen.natour@serli.com>
2024-10-22 17:45:56 +0200

    Update index.js

--- routes/github/index.js
+++ routes/github/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const GithubController = require('../../controllers/githubController');
-const authJWTMiddleware = require("../../middlewares/authTokenJWT");
 
 
-router.get('/get-repos', authJWTMiddleware() , async (req, res) => {
+router.get('/get-repos',  async (req, res) => {
     const repos = await GithubController.getRepos(req.connectedUser.gitToken);
     res.json(repos);
 })
 
-router.get('/get-branches', authJWTMiddleware() , async (req, res) => {
+router.get('/get-branches',async (req, res) => {
     const repo = req.query.repo;
     const branches = await GithubController.getBranches(req.connectedUser, repo);
     res.json(branches);
