4ecd07ecd8b70cbded7f07b5be7d135368fcc7e5
Mazen-Serli <mazen.natour@serli.com>
2024-10-23 11:43:27 +0200

    FlagFile
    add flag file

--- controllers/githubController.js
+++ controllers/githubController.js
@@ -19,9 +19,9 @@ class GithubController {
                     visibility: 'all',
                 }
             });
-            return {repos: response.data, error_code: response.status, message: ""};
+            return {repos: response.data, response_code: response.status, message: ""};
         } catch (error) {
-            return {repos: [], error_code: error.response.status, message: error.response.data.message};
+            return {repos: [], response_code: error.response.status, message: error.response.data.message};
         }
     }
 
@@ -32,9 +32,32 @@ class GithubController {
                     'Authorization': `Bearer ${connectedUser.gitToken}`
                 }
             });
-            return {branches: response.data, error_code: response.status, message: ""};
+            return {branches: response.data, response_code: response.status, message: ""};
         } catch (error) {
-            return {branches: [], error_code: error.response.status, message: error.response.data.message};
+            return {branches: [], response_code: error.response.status, message: error.response.data.message};
+        }
+    }
+
+    static async getFlagFile(connectedUser, reqInfo) {
+        const { repoOwner, repoName, filePath, branch } = reqInfo;
+
+        if (!repoOwner || !repoName || !filePath) {
+            return {FlagFile: {}, response_code: 400, message: 'Missing repoOwner, repoName, or filePath query parameter'};
+        }
+
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`, {
+                headers: {
+                    Authorization: `Bearer ${connectedUser.gitToken}`,
+                    Accept: 'application/vnd.github.v3.raw' // Ce header assure que le contenu du fichier est retourné en texte brut
+                },
+                params: {
+                    ref: branch || 'main' // Utilise la branche spécifiée ou 'main' par défaut
+                }
+            });
+            return {FlagFile: response.data, response_code: response.status, message: ""};
+        } catch (error) {
+            return {FlagFile: {}, response_code: error.response.status, message: error.response.data.message};
         }
     }
 }
--- routes/github/index.js
+++ routes/github/index.js
@@ -4,18 +4,18 @@ const GithubController = require('../../controllers/githubController');
 
 
 router.get('/get-repos',  async (req, res) => {
-    const {repos, error_code, message} = await GithubController.getRepos(req.connectedUser.gitToken);
-    if(error_code >= 400) {
-        return res.status(error_code).json({message: message});
+    const {repos, response_code, message} = await GithubController.getRepos(req.connectedUser.gitToken);
+    if(response_code >= 400) {
+        return res.status(response_code).json({message: message});
     }
     res.json(repos);
 })
 
 router.get('/get-branches',async (req, res) => {
     const repo = req.query.repo;
-    const {branches, error_code, message} = await GithubController.getBranches(req.connectedUser, repo);
-    if(error_code >= 400) {
-        return res.status(error_code).json({message: message});
+    const {branches, response_code, message} = await GithubController.getBranches(req.connectedUser, repo);
+    if(response_code >= 400) {
+        return res.status(response_code).json({message: message});
     }
     return res.json(branches);
 })
