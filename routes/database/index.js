c0dc7b06c01b475458bedf24fd0122a079c7ca66
Mazen-Serli <mazen.natour@serli.com>
2024-10-24 10:01:13 +0200

    Update index.js

--- routes/database/index.js
+++ routes/database/index.js
@@ -6,7 +6,7 @@ const projectController = require("../../database/controllers/projectController"
 const userController = require("../../database/controllers/userController");
 
 router.post('/save-flag-file',  async (req, res) => {
-    const {flag_file, filetype, repo, branch} = req.body;
+    const {flag_file, filetype, repoName, branch} = req.body;
     const { isFlagFile, flags} = flagsValidatorController.isValidFlagFile(flag_file, filetype);
 
     if(!isFlagFile){
@@ -14,7 +14,7 @@ router.post('/save-flag-file',  async (req, res) => {
     }
 
     const user = await userController.getUser(req.connectedUser.name);
-    const project =await projectController.getIfExistOrCreate(user.id, repo);
+    const project = await projectController.getIfExistOrCreate(user.id, repoName);
     const flag = await flagsController.createFlag(project.id, branch, flags);
 
     res.json(flag.id);
