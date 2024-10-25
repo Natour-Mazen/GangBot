4a254846dbb57c86a631ab83f70de218ca9bffc1
Mazen-Serli <mazen.natour@serli.com>
2024-10-25 14:31:53 +0200

    Update index.js
    fix some routes path

--- routes/github/index.js
+++ routes/github/index.js
@@ -5,24 +5,24 @@ const AuthController = require('../../controllers/authController');
 const handleResponse = require("../../handlers/responseHandler");
 
 
-router.get('/get-user', async (req, res) => {
+router.get('/user', async (req, res) => {
     const {userData, response_code, message} = await AuthController.getUserProfileInfos(req.connectedUser);
     handleResponse(res, userData, response_code, message);
 })
 
 
-router.get('/get-repos',  async (req, res) => {
+router.get('/repos',  async (req, res) => {
     const {repos, response_code, message} = await GithubController.getRepos(req.connectedUser);
     handleResponse(res, repos, response_code, message);
 })
 
-router.get('/get-branches',async (req, res) => {
+router.get('/branches',async (req, res) => {
     const repoName = req.query.repoName;
     const {branches, response_code, message} = await GithubController.getBranches(req.connectedUser, repoName);
     handleResponse(res, branches, response_code, message);
 })
 
-router.get("/get-flag-file", async (req, res) => {
+router.get("/flag-file", async (req, res) => {
     const {repoName, branch, all_branch} = req.query;
     const connectedUser = req.connectedUser;
     let flagFiles = [];
