ecc912f604215ce2f9f2b2fd8bb95de9ae2e7b4a
Mazen-Serli <mazen.natour@serli.com>
2024-10-30 17:14:36 +0100

    Update validateJWTMiddleware.js
    update the middleware

--- middlewares/validateJWTMiddleware.js
+++ middlewares/validateJWTMiddleware.js
@@ -2,64 +2,57 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const UserController = require('../database/controllers/userController');
 const GroupController = require('../database/controllers/groupController');
+const ProviderType = require("../providers/Types");
 
 // Load environment variables from .env file
 dotenv.config();
 const { JWT_SECRET_KEY, APP_MODE } = process.env;
 
-const validateJWTMiddleware = () => (req, res, next) => {
-    // Get the 'access Token' cookie from the request
+const handleErrorResponse = (res, message, status = 401) => {
+    if (APP_MODE === 'Dev') {
+        return res.status(status).json({ message });
+    } else if (APP_MODE === 'Prod') {
+        return res.redirect('/auth');
+    } else {
+        throw new Error('APP_MODE not set');
+    }
+};
+
+const getUserGroups = async (userId) => {
+    const groups = await GroupController.getGroupsNamesByUserId(userId);
+    return groups.map(group => group.dataValues.groupname);
+};
+
+
+const validateJWTMiddleware = () => async (req, res, next) => {
     const accessToken = req.cookies.ff_access_token;
 
     if (!accessToken) {
-       // console.log('No access token');
-        if(APP_MODE === 'Dev') {
-            return res.status(401).json({ message: 'No access token' });
-        }else if(APP_MODE === 'Prod'){
-            return res.redirect('/auth');
-        }else {
-            throw new Error('APP_MODE not set');
-        }
-
+        return handleErrorResponse(res, 'No access token');
     }
 
-    // Verify the token
     jwt.verify(accessToken, JWT_SECRET_KEY, async (err, decoded) => {
         if (err) {
-            if(APP_MODE === 'Dev') {
-                return res.status(401).json({message: 'Invalid or expired token'});
-            }else if(APP_MODE === 'Prod'){
-                return res.redirect('/auth');
-            }else {
-                throw new Error('APP_MODE not set');
-            }
+            return handleErrorResponse(res, 'Invalid or expired token');
         }
 
-        const db_User = await UserController.getUserByGitHubId(decoded.githubID);
+        const db_User = await UserController.getUserById(decoded.id);
 
-        if(!db_User){
-            if(APP_MODE === 'Dev') {
-                return res.status(401).json({message: 'User not found'});
-            }else if(APP_MODE === 'Prod'){
-                return res.redirect('/auth');
-            }else {
-                throw new Error('APP_MODE not set');
-            }
+        if (!db_User) {
+            return handleErrorResponse(res, 'User not found');
         }
 
-        //get user's groups
-        const groups = await GroupController.getGroupsNamesByUserId(db_User.dataValues.id);
+        const groups = await getUserGroups(db_User.dataValues.id);
 
-        //Add the user to the request to use it more easily
         req.connectedUser = {
-            id : db_User.dataValues.id,
-            gitID: decoded.githubID,
-            gitName: decoded.githubName,
-            gitToken: decoded.githubToken,
-            groups: groups.map(group => group.dataValues.groupname),
+            id: db_User.dataValues.id,
+            vcsID: decoded.vcsID,
+            vcsName: decoded.vcsName,
+            vcsToken: decoded.vcsToken,
+            vcsProvider: decoded.vcsProvider,
+            groups,
         };
 
-        // Move to the next function (middleware or route manager)
         next();
     });
 };
