bd01d55ba279ee46570f1e8f316efb8b76d88173
Mazen-Serli <mazen.natour@serli.com>
2024-10-30 17:16:09 +0100

    Routes
    add new routes

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const AuthController = require('../../controllers/authController');
+const GithubRouter = require('./githubIndex');
 
-
-router.get('/', (req, res) => {
-  const githubAuthUrl = AuthController.getGithubAuthUrl();
-  res.redirect(githubAuthUrl);
-});
-
-router.get('/callback', async (req, res) => {
-  const code = req.query.code;
-
-  if (!code) {
-    return res.status(400).json({ error: 'Github code is missing, try again' });
-  }
-
-  try {
-    const accessToken = await AuthController.handleOAuthCallback(code);
-    // console.log(accessToken);
-    res.cookie('ff_access_token', accessToken, { httpOnly: true, secure: true, sameSite: 'None' });
-    //res.json({ message: 'Access token stored in cookie' });
-    res.redirect('/');
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch access token' });
-    console.log(error);
-  }
-});
+router.use('/github', GithubRouter);
 
 router.get('/logout', (req, res) => {
-  res.clearCookie('ff_access_token', { path: '/', sameSite: 'None', secure: true });
-  res.json({ message: 'Logged out successfully' });
+    res.clearCookie('ff_access_token', { path: '/', sameSite: 'None', secure: true });
+    res.json({ message: 'Logged out successfully' });
 });
 
 module.exports = router;
