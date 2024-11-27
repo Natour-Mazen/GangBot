40d2fa3ba30e4004d3ab5fb466144c04bfee48ce
Mazen-Serli <mazen.natour@serli.com>
2024-11-27 11:53:55 +0100

    Rename

--- routes/database/index.js
+++ routes/database/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const projectsRouter = require('./projectIndex');
-const flagRouter = require('./flagIndex');
+const projectsRouter = require('./projectsIndex');
+const flagsRouter = require('./flagsIndex');
 const providersRouter = require('./providersIndex');
 
 router.use('/projects', projectsRouter);
-router.use('/flags', flagRouter);
+router.use('/flags', flagsRouter);
 router.use('/providers', providersRouter);
 
 module.exports = router;
