45d2d8417c1129383aace17fd43950768a5b2b55
Mazen-Serli <mazen.natour@serli.com>
2024-11-28 16:19:28 +0100

    Update index.js

--- routes/database/index.js
+++ routes/database/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const projectsRouter = require('./projects');
-const flagsRouter = require('./flags');
 const providersRouter = require('./providers');
+const organizationsRouter = require('./organizations');
 
 router.use('/projects', projectsRouter);
-router.use('/flags', flagsRouter);
 router.use('/providers', providersRouter);
+router.use('/organizations', organizationsRouter);
 
 module.exports = router;
