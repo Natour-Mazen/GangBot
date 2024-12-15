c9c800b591d265c338932f7cb7ac027b555be1f0
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-15 13:01:54 +0100

    Update alternantsRappelEvent.js
    fix crone time

--- src/scheduleEvents/events/rappelMessageEventTypes/alternantsRappelEvent.js
+++ src/scheduleEvents/events/rappelMessageEventTypes/alternantsRappelEvent.js
@@ -6,6 +6,6 @@ export default class AlternantsRappelEvent extends RappelMessageEvent {
         const channelId = "1315307101723033645"; // ID du salon #général-étude-m2
         const lesAlternantsRole = "<@&1315307100485980224>";
         const message = `Bonjour ${lesAlternantsRole}, on est à la fin du mois, n'oubliez pas de remplir votre fiche de présence et l'envoyer à Sylvie, sinon Hakim va pas être content !`;
-        super(client, "AlternantRappel", "0 0 9 28 *", null, channelId, message);
+        super(client, "AlternantRappel", "0 9 28 * *", null, channelId, message);
     }
 }
