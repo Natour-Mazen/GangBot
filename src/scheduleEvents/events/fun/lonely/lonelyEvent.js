16968b793e1c7f8820c2cbaf87ecb4d25c421779
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-15 19:21:46 +0100

    Lonley troll event
    fix it and add some new msgs

---
+++ src/scheduleEvents/events/fun/lonely/lonelyEvent.js
@@ -0,0 +1,47 @@
+import BaseEvent from '../../../baseEvent.js';
+import MyRecurrenceRule from "../../../myRecurrenceRule.js";
+import fs from "fs";
+
+export default class LonelyEvent extends BaseEvent {
+    constructor(client) {
+        const trollRecurrenceRule = MyRecurrenceRule.everyXMinutes(15);
+        super(client, "LonelyEvent", trollRecurrenceRule);
+        this.channelId = "1317561997847691274";
+    }
+
+    async execute() {
+        try {
+            const guild = this.client.guilds.cache.first(); // Récupère la première guilde (serveur)
+            if (!guild) {
+                console.error('Aucune guilde trouvée.');
+                return;
+            }
+
+
+            // Récupère tous les membres en ligne
+            const onlineMembers = guild.members.cache.filter(member =>
+                (member.presence?.status === 'online' || member.presence?.status === 'dnd')
+                && !member.user.bot
+            );
+
+            if (onlineMembers.size === 1 && Math.floor(Math.random() * 30) === 0) {
+                const channel = await this.client.channels.fetch(this.channelId);
+                if (channel && channel.isTextBased()) {
+                    const lonelyMember = onlineMembers.first();
+                    // Lire le fichier JSON
+                    const messages = JSON.parse(fs.readFileSync('src/scheduleEvents/events/fun/lonely/lonelyMsgs.json', 'utf8'));
+                    const randomPhrase = messages[Math.floor(Math.random() * messages.length)];
+                    const message = `Hey ${lonelyMember}, ${randomPhrase}`;
+                    await channel.send(message);
+                   // console.log(`Message envoyé dans le salon ${this.channelId} : ${message}`);
+                } else {
+                    console.error(`Le salon ${this.channelId} n'est pas textuel ou introuvable.`);
+                }
+            } else {
+                // console.log('Pas de membre seul en ligne ou pas de chance pour le troll.');
+            }
+        } catch (error) {
+            console.error(`Erreur lors de l'exécution de ${this.name} pour le salon ${this.channelId}:`, error);
+        }
+    }
+}
