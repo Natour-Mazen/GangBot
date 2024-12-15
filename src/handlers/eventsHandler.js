84a363e1e6b1127081fa55991c65f07411040b6b
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-15 19:55:45 +0100

    Revert "tree work rework"
    This reverts commit 396aa5648d1ecb006af3d9b40ec6427323f0c6f4.

--- src/handlers/eventsHandler.js
+++ src/handlers/eventsHandler.js
@@ -1,8 +1,8 @@
 // eventsHandler.js
-import AlternantsRappelEvent from "../events/useful/rappelMessageEventTypes/alternantsRappelEvent.js";
-import StagiairesRappelEvent from "../events/useful/rappelMessageEventTypes/stagiairesRappelEvent.js";
+import AlternantsRappelEvent from "../scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js";
+import StagiairesRappelEvent from "../scheduleEvents/events/useful/rappelMessageEventTypes/stagiairesRappelEvent.js";
 import EventScheduler from "../scheduleEvents/eventScheduler.js";
-import LonelyEvent from "../events/fun/lonely/lonelyEvent.js";
+import LonelyEvent from "../scheduleEvents/events/fun/lonely/lonelyEvent.js";
 
 
 class EventsHandler {
