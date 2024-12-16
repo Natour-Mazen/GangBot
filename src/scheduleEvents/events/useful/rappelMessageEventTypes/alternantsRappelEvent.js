ecfa67d9b0aab22706397b93b7605cbee55fcaa7
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-16 21:24:08 +0100

    Update alternantsRappelEvent.js
    fix the recurrenceRule to the last day of each month

--- src/scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js
+++ src/scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js
@@ -8,7 +8,17 @@ export default class AlternantsRappelEvent extends RappelMessageEvent {
         const channelId = "1315307101723033645"; // ID du salon #général-étude-m2
         const lesAlternantsRole = "<@&1315307100485980224>";
         const message = `Bonjour ${lesAlternantsRole}, on est à la fin du mois, n'oubliez pas de remplir votre fiche de présence et l'envoyer à Sylvie, sinon Hakim va pas être content !`;
-        const recurrenceRule = new MyRecurrenceRule(0, 0, 10, 28, "*", "*", "*", "*");
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth(); // (0 = janvier)
+        const lastDay = AlternantsRappelEvent.getLastDayOfMonth(year, month);
+        const recurrenceRule = new MyRecurrenceRule(0, 0, 10, `${lastDay}`, "*", "*", "*", "*");
         super(client, "AlternantRappel", recurrenceRule, channelId, message);
     }
+
+    static getLastDayOfMonth = (year, month) => {
+        return new Date(year, month + 1, 0).getDate(); // Le 0ème jour du mois suivant donne le dernier jour du mois actuel
+    };
+
+
 }
