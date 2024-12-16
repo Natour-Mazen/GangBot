0d3e259ed1e163d0711c10dd089becf957582f69
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-16 22:32:59 +0100

    Update alternantsRappelEvent.js
    calcule the exacte last day of the month

--- src/scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js
+++ src/scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js
@@ -1,4 +1,3 @@
-
 import RappelMessageEvent from "../rappelMessageEvent.js";
 import MyRecurrenceRule from "../../../myRecurrenceRule.js";
 
@@ -8,17 +7,19 @@ export default class AlternantsRappelEvent extends RappelMessageEvent {
         const channelId = "1315307101723033645"; // ID du salon #général-étude-m2
         const lesAlternantsRole = "<@&1315307100485980224>";
         const message = `Bonjour ${lesAlternantsRole}, on est à la fin du mois, n'oubliez pas de remplir votre fiche de présence et l'envoyer à Sylvie, sinon Hakim va pas être content !`;
-        const now = new Date();
-        const year = now.getFullYear();
-        const month = now.getMonth(); // (0 = janvier)
-        const lastDay = AlternantsRappelEvent.getLastDayOfMonth(year, month);
-        const recurrenceRule = new MyRecurrenceRule(0, 0, 10, `${lastDay}`, "*", "*", "*", "*");
-        super(client, "AlternantRappel", recurrenceRule, channelId, message);
+        const recurrenceRule = AlternantsRappelEvent.calculateRecurrenceRule();
+        super(client, "AlternantRappel", recurrenceRule,true, channelId, message);
     }
 
     static getLastDayOfMonth = (year, month) => {
         return new Date(year, month + 1, 0).getDate(); // Le 0ème jour du mois suivant donne le dernier jour du mois actuel
     };
 
-
+    static calculateRecurrenceRule() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth(); // (0 = janvier)
+        const lastDay = AlternantsRappelEvent.getLastDayOfMonth(year, month);
+        return new MyRecurrenceRule(0, 30, 10, lastDay, "*", "*", "*", "*");
+    }
 }
