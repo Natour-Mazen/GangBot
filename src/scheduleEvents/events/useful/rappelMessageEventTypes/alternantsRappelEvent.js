527fb4ca949c4808c953822dcb8e92b3b489290d
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-21 17:59:49 +0100

    Update alternantsRappelEvent.js
    fix bug to recalculate the event for the next month and not the actual one

--- src/scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js
+++ src/scheduleEvents/events/useful/rappelMessageEventTypes/alternantsRappelEvent.js
@@ -7,19 +7,27 @@ export default class AlternantsRappelEvent extends RappelMessageEvent {
         const channelId = "1315307101723033645"; // ID du salon #général-étude-m2
         const lesAlternantsRole = "<@&1315307100485980224>";
         const message = `Bonjour ${lesAlternantsRole}, on est à la fin du mois, n'oubliez pas de remplir votre fiche de présence et l'envoyer à Sylvie, sinon Hakim va pas être content !`;
-        const recurrenceRule = AlternantsRappelEvent.calculateRecurrenceRule();
+        const recurrenceRule = [];
+        recurrenceRule.push(AlternantsRappelEvent.calculateRecurrenceRule());
+        recurrenceRule.push(new MyRecurrenceRule(0, 31, 10, 31, 11, 2024, "*", "*"));
         super(client, "AlternantRappel", recurrenceRule,true, channelId, message);
     }
 
     static getLastDayOfMonth = (year, month) => {
-        return new Date(year, month + 1, 0).getDate(); // Le 0ème jour du mois suivant donne le dernier jour du mois actuel
+        return new Date(year, month, 0).getDate(); // Le 0ème jour du mois suivant donne le dernier jour du mois actuel
     };
 
     static calculateRecurrenceRule() {
         const now = new Date();
-        const year = now.getFullYear();
-        const month = now.getMonth(); // (0 = janvier)
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1; // Passer au mois suivant
+
+        // Gérer le passage à l'année suivante si le mois est décembre
+        if (month > 11) {
+            month = 0; // Janvier de l'année suivante
+            year += 1;
+        }
         const lastDay = AlternantsRappelEvent.getLastDayOfMonth(year, month);
-        return new MyRecurrenceRule(0, 30, 10, lastDay, "*", "*", "*", "*");
+        return new MyRecurrenceRule(0, 30, 10, lastDay, month, year, "*", "*");
     }
 }
