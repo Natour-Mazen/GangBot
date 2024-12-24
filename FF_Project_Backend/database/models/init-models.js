3774f9dc44f44de905bbce39b51b5eb57012c711
Mazen-Serli <mazen.natour@serli.com>
2024-12-24 15:12:03 +0100

    Backend
    rework on the way we install the models to have more accurate and understandable things

--- FF_Project_Backend/database/models/init-models.js
+++ FF_Project_Backend/database/models/init-models.js
@@ -7,61 +7,92 @@ const _projects = require("./projects");
 const _providermethods = require("./providermethods");
 const _usergroups = require("./usergroups");
 const _users = require("./users");
-const _usertokens = require("./usertokens");
+const _authuserstokens = require("./authUsersTokens");
+const _providersauthuserstokens = require("./providersAuthUsersTokens");
 
-function initModels(sequelize) {
-  const accessgroup = _accessgroup(sequelize, DataTypes);
-  const organization = _organization(sequelize, DataTypes);
-  const organizationprojects = _organizationprojects(sequelize, DataTypes);
-  const organizationusers = _organizationusers(sequelize, DataTypes);
-  const projects = _projects(sequelize, DataTypes);
-  const providermethods = _providermethods(sequelize, DataTypes);
-  const usergroups = _usergroups(sequelize, DataTypes);
-  const users = _users(sequelize, DataTypes);
-  const usertokens = _usertokens(sequelize, DataTypes);
-
-  organization.belongsToMany(projects, { as: 'projectid_projects', through: organizationprojects, foreignKey: "organizationid", otherKey: "projectid" });
-  organization.belongsToMany(users, { as: 'userid_users', through: organizationusers, foreignKey: "organizationid", otherKey: "userid" });
-  projects.belongsToMany(organization, { as: 'organizationid_organizations', through: organizationprojects, foreignKey: "projectid", otherKey: "organizationid" });
-  users.belongsToMany(organization, { as: 'organizationid_organization_organizationusers', through: organizationusers, foreignKey: "userid", otherKey: "organizationid" });
-  users.belongsToMany(users, { as: 'userid_users_usergroups', through: usergroups, foreignKey: "groupid", otherKey: "userid" });
-  users.belongsToMany(users, { as: 'groupid_users', through: usergroups, foreignKey: "userid", otherKey: "groupid" });
-  users.belongsTo(providermethods, { as: "creationusermethod_providermethod", foreignKey: "creationusermethod"});
-  organizationprojects.belongsTo(organization, { as: "organization", foreignKey: "organizationid"});
-  organization.hasMany(organizationprojects, { as: "organizationprojects", foreignKey: "organizationid"});
-  organizationusers.belongsTo(organization, { as: "organization", foreignKey: "organizationid"});
-  organization.belongsTo(users, { as: "user", foreignKey: "creatorid"});
-  users.hasMany(organization, { as: "organization", foreignKey: "creatorid"});
-  organization.hasMany(organizationusers, { as: "organizationusers", foreignKey: "organizationid"});
-  organizationprojects.belongsTo(projects, { as: "project", foreignKey: "projectid"});
-  projects.hasMany(organizationprojects, { as: "organizationprojects", foreignKey: "projectid"});
-  projects.belongsTo(providermethods, { as: "authenticationmethod_providermethod", foreignKey: "importmethodid"});
-  providermethods.hasMany(projects, { as: "projects", foreignKey: "importmethodid"});
-  usertokens.belongsTo(providermethods, { as: "authenticationmethod_providermethod", foreignKey: "authenticationmethod"});
-  providermethods.hasMany(usertokens, { as: "usertokens", foreignKey: "authenticationmethod"});
-  organizationusers.belongsTo(users, { as: "user", foreignKey: "userid"});
-  users.hasMany(organizationusers, { as: "organizationusers", foreignKey: "userid"});
-  projects.belongsTo(users, { as: "user", foreignKey: "userid"});
-  users.hasMany(projects, { as: "projects", foreignKey: "userid"});
-  usergroups.belongsTo(users, { as: "group", foreignKey: "groupid"});
-  users.hasMany(usergroups, { as: "usergroups", foreignKey: "groupid"});
-  usergroups.belongsTo(users, { as: "user", foreignKey: "userid"});
-  users.hasMany(usergroups, { as: "user_usergroups", foreignKey: "userid"});
-  usertokens.belongsTo(users, { as: "user", foreignKey: "userid"});
-  users.hasMany(usertokens, { as: "usertokens", foreignKey: "userid"});
-
-  return {
-    accessgroup,
+function defineAssociations(models) {
+  const {
     organization,
+    projects,
+    users,
+    providermethods,
     organizationprojects,
     organizationusers,
-    projects,
-    providermethods,
     usergroups,
-    users,
-    usertokens,
-  };
+    authuserstokens,
+    providersauthuserstokens,
+  } = models;
+
+  const belongsToManyMappings = [
+    { source: organization, target: projects, through: organizationprojects, foreignKey: "organizationid", otherKey: "projectid", alias: "projectid_projects" },
+    { source: organization, target: users, through: organizationusers, foreignKey: "organizationid", otherKey: "userid", alias: "userid_users" },
+    { source: projects, target: organization, through: organizationprojects, foreignKey: "projectid", otherKey: "organizationid", alias: "organizationid_organizations" },
+    { source: users, target: organization, through: organizationusers, foreignKey: "userid", otherKey: "organizationid", alias: "organizationid_organization_organizationusers" },
+    { source: users, target: users, through: usergroups, foreignKey: "groupid", otherKey: "userid", alias: "userid_users_usergroups" },
+    { source: users, target: users, through: usergroups, foreignKey: "userid", otherKey: "groupid", alias: "groupid_users" },
+  ];
+
+  const belongsToMappings = [
+    { source: users, target: providermethods, foreignKey: "creationusermethod", alias: "creationusermethod_providermethod" },
+    { source: organizationprojects, target: organization, foreignKey: "organizationid", alias: "organization" },
+    { source: organizationprojects, target: projects, foreignKey: "projectid", alias: "project" },
+    { source: projects, target: providermethods, foreignKey: "importmethodid", alias: "authenticationmethod_providermethod" },
+    { source: authuserstokens, target: providermethods, foreignKey: "authenticationmethod", alias: "authenticationmethod_providermethod" },
+    { source: providersauthuserstokens, target: providermethods, foreignKey: "providermethod", alias: "providerauthenticationmethod_providermethod" },
+    { source: organizationusers, target: users, foreignKey: "userid", alias: "user" },
+    { source: projects, target: users, foreignKey: "userid", alias: "user" },
+    { source: usergroups, target: users, foreignKey: "groupid", alias: "group" },
+    { source: usergroups, target: users, foreignKey: "userid", alias: "user" },
+    { source: authuserstokens, target: users, foreignKey: "userid", alias: "user" },
+  ];
+
+  const hasManyMappings = [
+    { source: organization, target: organizationprojects, foreignKey: "organizationid", alias: "organizationprojects" },
+    { source: organization, target: organizationusers, foreignKey: "organizationid", alias: "organizationusers" },
+    { source: projects, target: organizationprojects, foreignKey: "projectid", alias: "organizationprojects" },
+    { source: providermethods, target: projects, foreignKey: "importmethodid", alias: "projects" },
+    { source: providermethods, target: authuserstokens, foreignKey: "authenticationmethod", alias: "authuserstokens" },
+    { source: providermethods, target: providersauthuserstokens, foreignKey: "providermethod", alias: "providersauthuserstokens" },
+    { source: users, target: organization, foreignKey: "creatorid", alias: "organization" },
+    { source: users, target: organizationusers, foreignKey: "userid", alias: "organizationusers" },
+    { source: users, target: projects, foreignKey: "userid", alias: "projects" },
+    { source: users, target: usergroups, foreignKey: "groupid", alias: "usergroups" },
+    { source: users, target: usergroups, foreignKey: "userid", alias: "user_usergroups" },
+    { source: users, target: authuserstokens, foreignKey: "userid", alias: "authuserstokens" },
+  ];
+
+  belongsToManyMappings.forEach(({ source, target, through, foreignKey, otherKey, alias }) => {
+    source.belongsToMany(target, { as: alias, through, foreignKey, otherKey });
+  });
+
+  belongsToMappings.forEach(({ source, target, foreignKey, alias }) => {
+    source.belongsTo(target, { as: alias, foreignKey });
+  });
+
+  hasManyMappings.forEach(({ source, target, foreignKey, alias }) => {
+    source.hasMany(target, { as: alias, foreignKey });
+  });
 }
+
+function initModels(sequelize) {
+  const models = {
+    accessgroup: _accessgroup(sequelize, DataTypes),
+    organization: _organization(sequelize, DataTypes),
+    organizationprojects: _organizationprojects(sequelize, DataTypes),
+    organizationusers: _organizationusers(sequelize, DataTypes),
+    projects: _projects(sequelize, DataTypes),
+    providermethods: _providermethods(sequelize, DataTypes),
+    usergroups: _usergroups(sequelize, DataTypes),
+    users: _users(sequelize, DataTypes),
+    authuserstokens: _authuserstokens(sequelize, DataTypes),
+    providersauthuserstokens: _providersauthuserstokens(sequelize, DataTypes),
+  };
+
+  defineAssociations(models);
+
+  return models;
+}
+
 module.exports = initModels;
 module.exports.initModels = initModels;
 module.exports.default = initModels;
