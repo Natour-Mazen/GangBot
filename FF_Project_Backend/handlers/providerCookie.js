904aef77f09f47eebbd5431d2e7b0b26e91d5e9a
Mazen-Serli <mazen.natour@serli.com>
2024-12-24 15:17:11 +0100

    Update providerCookie.js
    rename handlers

--- FF_Project_Backend/handlers/providerCookie.js
+++ FF_Project_Backend/handlers/providerCookie.js
@@ -1,33 +1,22 @@
 // Description: Handler for setting and unsetting provider cookie.
-const {sign} = require("jsonwebtoken");
-const dotenv = require('dotenv');
-dotenv.config();
+const COOKIE_NAME = 'ff_provider_token';
 
-const { JWT_SECRET_KEY } = process.env;
 
-const generateJWTProviderToken = (token, userid, username, type) => {
-    return sign({ userid, username, token, type }, JWT_SECRET_KEY, {
-        expiresIn: '1d'
-    });
-}
+const getProviderCookieValue = (req) => {
+    return req.cookies[COOKIE_NAME];
+};
 
-const setProviderCookieHandler = (res, token, userid, username, type) => {
-    console.log('Setting provider cookie');
-    console.log('Token:', token);
-    console.log('Type:', type);
-    console.log('Userid:', userid);
-    console.log('Username:', username);
-    const providerToken = generateJWTProviderToken(token, userid, username, type);
-
-    res.cookie('ff_provider_token', providerToken, { httpOnly: true, secure: true, sameSite: 'None' });
+const setProviderCookie = async (res, token) => {
+    res.cookie(COOKIE_NAME, token, {httpOnly: true, secure: true, sameSite: 'None'});
     res.redirect('/');
 };
 
-const unSetProviderCookieHandler = (res) => {
-    res.clearCookie('ff_provider_token', {path: '/', sameSite: 'None', secure: true});
+const unSetProviderCookie = (res) => {
+    res.clearCookie(COOKIE_NAME, {path: '/', sameSite: 'None', secure: true});
 }
 
 module.exports = {
-    setProviderCookieHandler,
-    unSetProviderCookieHandler
+    getProviderCookieValue,
+    setProviderCookie,
+    unSetProviderCookie
 };
