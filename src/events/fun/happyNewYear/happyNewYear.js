8eff9ab43868f475ccd964f062dc549ae042bdba
Al Natour Mazen <mazen.natour.pro@gmail.com>
2025-01-01 16:17:10 +0100

    Update happyNewYear.js

--- src/events/fun/happyNewYear/happyNewYear.js
+++ src/events/fun/happyNewYear/happyNewYear.js
@@ -4,10 +4,22 @@ import fs from "fs";
 
 export default class HappyNewYearEvent extends BaseEvent {
     constructor(client) {
-        const newYearRecurrenceRule = new MyRecurrenceRule(0,0,0,1,0,"*","*");
+        const newYearRecurrenceRule = HappyNewYearEvent.defineRecurrenceRule();
         super(client, "HappyNewYearEvent", newYearRecurrenceRule, false);
         this.channelId = "1315307101093892159";
     }
+
+    static defineRecurrenceRule() {
+        const nowDate = new Date();
+        const isNewYear = nowDate.getDate() === 1 && nowDate.getMonth() === 0 && nowDate.getHours() === 0;
+        if (isNewYear) {
+            return new MyRecurrenceRule(0, 0, 0, 1, 0, nowDate.getFullYear(), "*");
+        }
+        // Calculer pour le prochain Nouvel An
+        const nextYear = nowDate.getFullYear() + 1;
+        return new MyRecurrenceRule(0, 0, 0, 1, 0, nextYear, "*");
+    }
+
 
     async execute() {
         console.log('HappyNewYearEvent execute()');
