baccf5a219748508b43fb94563959e2514fbf4a7
Al Natour Mazen <mazen.natour.pro@gmail.com>
2025-01-04 00:03:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/', (req, res) => {
         fetch(`https://gangbot.onrender.com`) // Ou l'URL déployée de votre bot
             .then(res => console.log(`Pinged /: ${res.status}`))
             .catch(err => console.error(`Failed to ping /:`, err));
-    }, 60000); // Ping toutes les 60 secondes
+    }, 3600000); // Ping toutes les heures
 
 });
 
