3d167b03615ecafad47240e6184350cf15c0c7c4
Al Natour Mazen <mazen.natour.pro@gmail.com>
2025-01-03 15:26:25 +0100

    Update locateCommande.js

--- src/commandes/useful/locate/locateCommande.js
+++ src/commandes/useful/locate/locateCommande.js
@@ -4,16 +4,19 @@ import fs from "fs";
 class LocateCommand extends BasicCommand {
     constructor() {
         super('locate', 'Renvoie une réponse spécifique en fonction du choix de l\'utilisateur');
+        const choicesFilePath = 'src/commandes/useful/locate/choices.json';
+        this.choices = fs.existsSync(choicesFilePath) ? JSON.parse(fs.readFileSync(choicesFilePath, 'utf8')) : {};
     }
 
     async handleCommand(interaction) {
         const userChoice = interaction.options.getString('choice');
-        const choices = JSON.parse(fs.readFileSync('src/commandes/useful/locate/choices.json', 'utf8'));
-        let response = choices[userChoice] || 'Choix non reconnu 🥲';
+        let response = this.choices[userChoice] || 'Choix non reconnu 🥲';
         await interaction.reply(response);
     }
 
     addOptionalCommandData() {
+        const choicesFilePath = 'src/commandes/useful/locate/choices.json';
+        const choices = fs.existsSync(choicesFilePath) ? JSON.parse(fs.readFileSync(choicesFilePath, 'utf8')) : {};
         this.getCommandData().addStringOption(option =>
             option.setName('choice')
                 .setDescription('Choix de l\'utilisateur')
